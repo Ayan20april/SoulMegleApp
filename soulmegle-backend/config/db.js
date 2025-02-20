c23d91786c218362481dc2dd4c1661ed9038b9ff
Ayan Mahanta <112053665+Ayan20april@users.noreply.github.com>
2025-02-20 07:29:13 +0000

    update for database

--- soulmegle-backend/config/db.js
+++ soulmegle-backend/config/db.js
@@ -2,16 +2,16 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false, // set to console.log if you want SQL logs
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false, // Change to console.log to see SQL logs
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Adjust based on your security requirements
+    },
+  },
+});
 
 module.exports = sequelize;
